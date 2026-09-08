3fde52b5337d43f0bfdf21b1ba24ad4ae5e8fb3e
here-is-what-happened <therealarjunvazir@gmail.com>
2026-09-08 18:22:27 +0000

    First Commit

---
+++ crash-course.py
@@ -0,0 +1,29 @@
+#comment
+
+print("Hello World!")
+
+
+# VARIABLE DECLARATION AND DATA TYPES:
+
+a = 4        #integer
+b = 5.5      #floats
+c = "CSAEA"  #strings
+d = True     #boolean
+
+
+print(a, b, c, d)
+
+
+# OPERATORS:
+# +, -, /, *, %, **, //
+
+e = 7+8
+print(e)
+
+e += 7
+print(e)
+
+
+# f-string
+
+print(f"e is equal to {e}")
