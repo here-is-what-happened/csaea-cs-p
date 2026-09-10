4975cf3e5e81563e2265088f9206b594ab9bd904
here-is-what-happened <therealarjunvazir@gmail.com>
2026-09-10 16:26:24 +0000

    second commit

--- crash-course.py
+++ crash-course.py
@@ -1,3 +1,6 @@
+import math
+
+
 #comment
 
 print("Hello World!")
@@ -27,3 +30,47 @@ print(e)
 # f-string
 
 print(f"e is equal to {e}")
+
+print(4 == 5)
+print(4 != 7)
+
+isEqual = "yes" == "yesss"
+print(isEqual)
+
+# LOGICAL OPERATORS
+# ORDER OF PRECEDENCE   - not, and, or
+
+f = False
+t = True
+
+print("            ")
+print(not f)
+print(f or t)
+print(f or t and not f)
+print(f and t)
+print("                           ")
+
+
+# CASTING ()
+
+g = int(5.5)
+print(g)
+print("          ")
+
+
+# STRINGS
+
+s1 = "Goodnight"
+s2 = " and "
+s3 = "Goodbye"
+end = s1 + s2 + s3 #concetanation with +
+
+end += ", Cowboy."
+
+print(end + "\n")
+
+# MATH LIBRARY
+
+print(math.sqrt(14))
+print(math.floor(3.65))
+print(math.pow(2, 4))
