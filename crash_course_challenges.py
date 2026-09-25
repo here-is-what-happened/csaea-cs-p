59df2417c84ac029b10d32baffd944d6f61a903c
here-is-what-happened <therealarjunvazir@gmail.com>
2026-09-25 18:23:25 +0000

    a

--- crash_course_challenges.py
+++ crash_course_challenges.py
@@ -1,3 +1,5 @@
+import math
+
 # Challenge 1, Tip generator
 
 bill = 50
@@ -33,3 +35,34 @@ print("The rocket has launched!")
 # challenge 12, times table, helper
 
 number = 7
+total = 0
+
+for i in range(1, 11, 1):
+    total += 7
+    print(f"{number} x {i} = {total}")
+
+# challenge 14, high score calculator
+scores = [340, 1250, 980, 1510, 720]
+
+high_score = max(scores)
+print(f"\nThe high score is {high_score}")
+
+# challenge 15, class pass rate
+grades = [88, 65, 72, 91, 54, 70]
+passing = 70
+passed = 0
+total_students = 0
+
+for grade in grades:
+    if grade >= 70:
+        passed += 1
+        total_students += 1
+    else:
+        total_students += 1
+print(f"\n{passed} out of {total_students} students passed the test")
+
+# challenge 16, garden fence
+
+area = 49
+amount_of_fence = 4*(math.sqrt(area))
+print(f"Fence needed in ft : {amount_of_fence}")
